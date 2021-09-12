76968fd565c4076841c4765ff486bb3e5a1326e4
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-09-12 11:27:58 +0400

    recipe master project completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,80 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+// {page: {
+//   name:
+//   sections: [{
+//     name:
+//     bgImage: {
+//       "check what happens when no image attached."
+//     }
+//     headings: [{
+//       type:
+//       content:
+//     }]
+//     imageCard: [{
+//       name:
+//       url:
+//       caption:
+//     }]
+//     richText: [{
+//       name:
+//       content:
+//     }]
+//   }]
+// }}
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "alex",
+    person: { name: "alex", age: 34 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 21 },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `fowgq4a7vlrq`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
+            },
+          ],
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 }
